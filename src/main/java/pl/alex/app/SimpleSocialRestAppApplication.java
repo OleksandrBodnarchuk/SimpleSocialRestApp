ee011a52b551196f4fcae70bef91eed786cbebee
OleksandrBodnarchuk <obodnarchuk@outlook.com>
2021-12-10 20:38:05 +0100

    Initial commit

---
+++ src/main/java/pl/alex/app/SimpleSocialRestAppApplication.java
@@ -0,0 +1,13 @@
+package pl.alex.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSocialRestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSocialRestAppApplication.class, args);
+    }
+
+}
